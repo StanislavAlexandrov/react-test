fd491832fd56ae7640225159b899ae1920a15a06
StanislavAlexandrov <stanislavalexandrov@gmail.com>
2021-01-17 18:43:20 +0000

    Deploying to gh-pages from  @ 48b19569ac57cd45546f0f2800864caafde35e34 🚀

---
+++ precache-manifest.3ffbf6498b92c3842e6052a47692248d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d975c7b2e30de204fef8b3df9a59f7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e4014531ab5484b4672",
+    "url": "/static/css/main.3e5a2f9b.chunk.css"
+  },
+  {
+    "revision": "d378c111379449bad790",
+    "url": "/static/js/2.0e9b89f4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0e9b89f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e4014531ab5484b4672",
+    "url": "/static/js/main.0fbe6313.chunk.js"
+  },
+  {
+    "revision": "1ca56b6e53ce1dc83ae6",
+    "url": "/static/js/runtime-main.0a3c20bd.js"
+  }
+]);
